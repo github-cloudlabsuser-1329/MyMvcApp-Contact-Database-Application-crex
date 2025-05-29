8a604885ce7a81b95f21fd2fda441a68469f568e
github_cloudlabsuser_1329@cloudlabsaiuser.com <github_cloudlabsuser_1329@cloudlabsaiuser.com>
2025-05-29 20:32:04 +0000

    added stuff

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b3ae40a7091a9f31260673e6d39d358f5608bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec03ded3adf8a2ddcb92db188eba96f37c950ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
