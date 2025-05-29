e3b3ae40a7091a9f31260673e6d39d358f5608bf
github_cloudlabsuser_1329@cloudlabsaiuser.com <github_cloudlabsuser_1329@cloudlabsaiuser.com>
2025-05-29 19:15:00 +0000

    github action workflow

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01f751434578b90b46ac6a982a5e25b6438a06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9feeae3c57c0cbc623103c44a92de0f5762ea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
